82fe402e4a996ed50b165b7453a5324f42cca63d
EC2 Default User <ec2-user@ip-172-31-27-226.us-east-2.compute.internal>
2018-03-05 11:34:13 +0000

    kadai-twitter1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 def index
   @tasks = Task.order(created_at: :desc).page(params[:page]).per(20)
