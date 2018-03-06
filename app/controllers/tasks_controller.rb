f4b030ed8b2f96cb89eaa8178fc16704a7b9bc78
EC2 Default User <ec2-user@ip-172-31-27-226.us-east-2.compute.internal>
2018-03-06 11:11:09 +0000

    tasklist-clone

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,57 +1,69 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-def index
-  @tasks = Task.order(created_at: :desc).page(params[:page]).per(20)
-end
-
-def show
-end
-
-def new
-  @task = Task.new
-end
-
-def create
-  @task = Task.new(task_params)
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
-  if @task.save
-    flash[:success] = 'Task が正常に投稿されました'
-    redirect_to @task
-  else
-    flash.now[:danger] = 'Task が投稿されませんでした'
+  def index
+    if logged_in?
+    @user = current_user
+    @task = current_user.tasks.build
+    @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(20)
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+      if @task.save
+        flash[:success] = 'Task が正常に投稿されました'
+        redirect_to @task
+      else
+        @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+        flash.now[:danger] = 'Task が投稿されませんでした'
     render :new
+      end
   end
-end
 
-def edit
-end
 
-def update
-  if @task.update(task_params)
-  flash[:success] = 'Task は正常に更新されました'
-  redirect_to @task
-  else
-    flash.now[:danger] = 'Task は更新されませんでした'
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+      if @task.update(task_params)
+        flash[:success] = 'Task は正常に更新されました'
+        redirect_to @task
+      else
+        flash.now[:danger] = 'Task は更新されませんでした'
     render :edit
+      end
   end
-end
 
-def destroy
-  @task.destroy
-  
-  flash[:success] = 'Task は正常に削除されました'
-  redirect_to tasks_url
-end
+  def destroy
+    @task.destroy
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
 
-private
 
-def set_task
-  @task = Task.find(params[:id])
-end
+  private
 
 # Strong Parameter
-def task_params
-  params.require(:task).permit(:content, :status)
-end
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to tasks_url
+    end
+  end
 end
